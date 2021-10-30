cdaae41c9926bea0e5289b67e21d9fc4de4d7b55
Alex981110 <alex.mtz873@gmail.com>
2021-10-29 19:46:25 -0500

    Add componente MostrarInfo

---
+++ src/views/mostrarInfo/MostrarInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MostrarInfo = () => {
+  return (
+    <div>
+      <div className={Classes.mainContainer}>
+        <h1>Iformacion capturada</h1>
+      </div>
+    </div>
+  )
+}
+
+export default MostrarInfo
