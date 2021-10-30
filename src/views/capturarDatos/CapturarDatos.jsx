d8abc9ada1bc43ce5d1a128579d9775ebfe8e095
Alex981110 <alex.mtz873@gmail.com>
2021-10-30 15:38:24 -0500

    [Add] comunicación entre componente hijos y redirección a vista detalles

--- src/views/capturarDatos/CapturarDatos.jsx
+++ src/views/capturarDatos/CapturarDatos.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Classes from './CapturarDatos.module.css'
 import DatosEmpresa from '../../Componentes/datosEmpresa/DatosEmpresa'
 import DatosUsuario from '../../Componentes/datosUsuario/DatosUsuario'
+import { useHistory } from 'react-router'
 
 const CapturarDatos = () => {
+
+  const history = useHistory();
+  const [doneCompany, setDoneCompany] = useState(false);
+  
+  const didUserFinish = () => {
+    history.push("/detallesInformacion");
+  }
+
+  const didCompanyFinish = (prop) => {
+    setDoneCompany(prop);
+  }
+
   return (
     <div className={Classes.mainContainer}>
       <div className={Classes.container}>
-        <DatosEmpresa />
+        <DatosEmpresa done={didCompanyFinish} />
       </div>
       <div className={Classes.container} style={{marginBottom: 40}}>
-        <DatosUsuario />
+        <DatosUsuario done={didUserFinish} formFilled={doneCompany}/>
       </div>
     </div>
   )
